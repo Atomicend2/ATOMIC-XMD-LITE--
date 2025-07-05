32d81a0d98bb2f9541a0ecef343fbf6429777b9a
Atomicend2 <mrmaize2@gmail.com>
2025-07-05 15:30:58 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,14 @@
-const { Low, JSONFile } = require('lowdb');
-const adapter = new JSONFile('./db.json');
-const db = new Low(adapter);
-(async () => await db.read())();
-module.exports = db;
+let db;
+
+async function loadDB() {
+  const { Low } = await import('lowdb');
+  const { JSONFile } = await import('lowdb/node');
+  const adapter = new JSONFile('./db.json');
+  db = new Low(adapter);
+  await db.read();
+  db.data ||= { users: [], sudo: [], autosend: '' };
+  await db.write();
+  return db;
+}
+
+module.exports = loadDB;
