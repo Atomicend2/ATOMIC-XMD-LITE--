fb71ad185ef5ddf0ab08b478e79781a9968b6bd3
Atomicend2 <mrmaize2@gmail.com>
2025-07-05 12:36:57 +0100

    Update autosend.js

--- autosend.js
+++ autosend.js
@@ -1,2 +1,12 @@
-// Auto poster to group
-module.exports = async () => { /* poster logic */ };
+module.exports = {
+  name: 'autosend',
+  category: 'Tools',
+  desc: 'Send a scheduled message to all groups',
+  use: '.autosend <text>',
+  async code(m, { sock, args, db }) {
+    const text = args.join(' ');
+    if (!text) return m.reply('Please provide text to send.');
+    db.data.autosend = text;
+    m.reply('Message set for auto send.');
+  }
+};
