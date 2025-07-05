276502e1625ea5f4580aa6e2560b56ff461346e4
Atomicend2 <mrmaize2@gmail.com>
2025-07-05 13:19:29 +0100

    Update tic.js

--- tic.js
+++ tic.js
@@ -1,2 +1,13 @@
-// Tic tac toe game
-module.exports = async () => { /* tic tac toe */ };
+module.exports = {
+  name: 'tic',
+  category: 'Games',
+  desc: 'Play a round of Tic-Tac-Toe',
+  async code(m) {
+    const board = [
+      '❌ | ⭕ | ❌',
+      '⭕ | ❌ | ⭕',
+      '❌ | ⭕ | ❌'
+    ].join('\n');
+    m.reply('🎮 *Tic-Tac-Toe*\n\n' + board + '\n\n(This is a static preview)');
+  }
+};
