0209ffc2dbb265ee221390df6b04306ff258da46
Atomicend2 <mrmaize2@gmail.com>
2025-07-05 13:15:52 +0100

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,1 +1,15 @@
-// snake.js plugin content
+module.exports = {
+  name: 'snake',
+  category: 'Games',
+  desc: 'Play a quick snake game (text version)',
+  async code(m) {
+    const board = [
+      '🟩🟩🟩🟩🟩',
+      '🟩🟢⬛⬛🟩',
+      '🟩⬛⬛⬛🟩',
+      '🟩⬛⬛⬛🟩',
+      '🟩🟩🟩🟩🟩'
+    ].join('\n');
+    m.reply('🐍 *Snake Game*\n\n' + board + '\n\n(Just a preview! Full game coming soon)');
+  }
+};
