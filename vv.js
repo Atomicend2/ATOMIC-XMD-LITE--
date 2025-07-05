092be1e64a9f80cd78c43a7822947c8011d930f2
Atomicend2 <mrmaize2@gmail.com>
2025-07-05 13:22:59 +0100

    Update vv.js

--- vv.js
+++ vv.js
@@ -1,1 +1,9 @@
-// vv.js plugin content
+module.exports = {
+  name: 'vv',
+  category: 'Tools',
+  desc: 'Bypass view-once media (if supported)',
+  use: '.vv',
+  async code(m) {
+    m.reply('⚠️ View-once media bypass will be supported in full release.\nStay tuned!');
+  }
+};
