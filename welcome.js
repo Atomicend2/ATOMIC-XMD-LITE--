4c4da8b6c428402156bcb68fe0add18d53cfef48
Atomicend2 <mrmaize2@gmail.com>
2025-07-05 13:26:31 +0100

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,1 +1,16 @@
-// welcome.js plugin content
+
+module.exports = {
+  name: 'welcome',
+  category: 'Group',
+  desc: 'Enable or disable welcome messages',
+  use: '.welcome on / .welcome off',
+  async code(m, { args, db }) {
+    if (!m.isGroup) return m.reply('This command can only be used in groups.');
+    const option = args[0];
+    if (!['on', 'off'].includes(option)) return m.reply('Usage: .welcome on / .welcome off');
+
+    db.data.welcome = db.data.welcome || {};
+    db.data.welcome[m.chat] = option === 'on';
+    m.reply(`Welcome message ${option === 'on' ? 'enabled' : 'disabled'} in this group.`);
+  }
+};
